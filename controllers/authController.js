ff1a2036b63efa48da9e9014319dfa4b14c5554a
Pierre Penel <penel.pierre@gmail.com>
2022-03-01 22:04:45 -0500

    auth: cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,8 +15,7 @@ const cookieOptions = {
   expires: new Date(
     Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
   ),
-  // httpOnly: false,
-  credentials: 'include',
+  httpOnly: true,
   sameSite: 'none',
   secure: false,
 };
@@ -67,16 +66,14 @@ exports.login = catchAsync(async (req, res, next) => {
 });
 
 exports.logout = catchAsync(async (req, res, next) => {
-  res.clearCookie();
+ 
+  res.cookie('user', 'null', {
+    expires: new Date(Date.now() -10 * 1000),
+    httpOnly: true,
+    sameSite: 'none',
+    secure: false,
+  });
   return res.status(200).json({ status: 'success' });
-
-  // res.cookie('user', 'null', {
-  //   expires: new Date(Date.now() -10 * 1000),
-  //   httpOnly: true,
-  //   sameSite: 'none',
-  //   secure: false,
-  // });
-  // return res.status(200).json({ status: 'success' });
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
