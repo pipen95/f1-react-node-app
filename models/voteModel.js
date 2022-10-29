62ad702701589d08c4c6e18c5faab37b38eda89b
Pierre Penel <penel.pierre@gmail.com>
2022-10-29 12:32:30 -0400

    Vote Model + Data mvp OK

--- models/voteModel.js
+++ models/voteModel.js
@@ -1,45 +1,38 @@
 const mongoose = require('mongoose');
-// const User = require("./userModel");
+// const User = require('./userModel');
 // const validator = require("validator");
 
 const voteSchema = new mongoose.Schema({
-  name: String,
-  driverId: String,
-  infoConsent: String,
-  rating: Number,
-  message: String,
-  country: {
-    // GeoJSON
-    type: {
-      type: String,
-      default: 'Point',
-      enum: ['Point'],
+  votedBy: { type: mongoose.Schema.ObjectId, ref: 'User' },
+  circuitId: String,
+  season: Number,
+  dateCreated: Date,
+  vote: [
+    {
+      id: String,
+      position: Number,
+      bonus: {
+        type: [String],
+      },
     },
-    coordinates: {
-      type: [Number],
-      default: [0, 0],
-    },
-    name: String,
-    iso: String,
-  },
-  region: {
-    // GeoJSON
-    type: {
-      type: String,
-      default: 'Point',
-      enum: ['Point'],
-    },
-    coordinates: {
-      type: [Number],
-      default: [0, 0],
-    },
-    name: String,
-    iso: String,
-  },
+  ],
 
-  city: String,
+  // Output
+  // vote: [{
+  //         "id": "albon",
+  //         "position": 10,
+  //         "bonus":["dod","ok","fl"]
+  //     }]
 });
 
 const Vote = mongoose.model('Vote', voteSchema);
 
 module.exports = Vote;
+
+// Populate Method
+// Then, when you make your query, you can populate references like this:
+// Post.findOne({_id: 123})
+// .populate('postedBy')
+// .exec(function(err, post) {
+//     // do stuff with post
+// });
