8034ac763c51f21688281552c99faa5ce0a6042b
Pierre Penel <penel.pierre@gmail.com>
2022-12-06 08:56:35 -0500

    result Controller

---
+++ controllers/resultController.js
@@ -0,0 +1,31 @@
+const Result = require('../models/voteModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.createResult = catchAsync(async (req, res, next) => {
+  const newResult = await Result.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      newResult,
+    },
+  });
+});
+
+exports.updateResult = catchAsync(async (req, res, next) => {
+  const updatedVote = await Result.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!updatedVote) {
+    return next(new AppError('No document found with that ID', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      updatedResult,
+    },
+  });
+});
